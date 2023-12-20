6cd838141314a68dbd86fd991f2e25aaed45af17
Agustin <agustin_cuelloluna01@hotmail.com>
2023-12-20 01:53:25 -0300

    vista crear (incompleto)

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -1,8 +1,13 @@
 let controllers = {
-    home: function(req,res){
-        res.render('producto')
+    home: function(req, res) {
+        res.render('producto');
+    },
+    edicion: function(req, res) {
+        res.render('edicion');
+    },
+    crear: function(req, res) {
+        res.render('crear');
     }
-}
-
+};
 
 module.exports = controllers;
