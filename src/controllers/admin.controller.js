c04c31c1211e0dba3a2685a98262b55145e0fa5a
Agustin <agustin_cuelloluna01@hotmail.com>
2024-02-28 02:47:05 -0300

    sprint 6 pt2

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -2,44 +2,51 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-const productsFilePath = path.join(__dirname, '../../data/productdb.json');
-let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+/* const productsFilePath = path.join(__dirname, '../../data/productdb.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); */
 
+const db = require('../database/models')
+const products = db.Products
+const id = db.Products.id
 
 
 
 let controllers = {
     home: function(req,res){
-        let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        db.Products.findAll()
+            .then ((products)=>{
         res.render('admin', {products:products})
+    })
     },
-    edit: function(req,res){
-        let id = req.params.id
-        let producto = products.find(product => product.id == id)
-        if(producto){
+    edit: async function(req,res){
+        try{
+            let producto = await db.Products.findByPk(req.params.id);
             return res.render('adminedit', {producto})
+        } catch (error) {
+            console.error(error);
         }
+        
+
     },
     update: function(req,res){
-        let id = req.params.id
-        let {name, desc, color, price} = req.body
-        console.log(req.body)
-        let product = products.find(product => product.id == id)
-        if(product){
-            product.name = name
-            product.desc = desc
-            product.color = color
-            product.price = price
-            fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '))
-            res.redirect('/admin')
-        } else{
-            res.send('El producto no se pudo actualizar')
-        }
+        db.Products.update({
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+            color: req.body.color
+        },  {
+            where: {
+                id: req.params.id
+            }
+        })
+        res.redirect('/admin')
     },
     delete: function(req,res){
-        let id = req.params.id
-        products = products.filter(product => product.id != id)
-        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '))
+        db.Products.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
         res.redirect('/admin')
     }
 }
